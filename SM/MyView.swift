c4131d0b687e082a9b97379574378ebf017cd28d
ptrk09 <48867712+ptrk09@users.noreply.github.com>
2019-09-16 00:42:06 +0300

    Add files via upload

---
+++ SM/MyView.swift
@@ -0,0 +1,22 @@
+//
+//  MyView.swift
+//  SM
+//
+//  Created by Paw.toporkov on 15/09/2019.
+//  Copyright © 2019 Paw.toporkov. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class MyView: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        ButtonCheck.drawPage5()
+    }
+    
+
+}
